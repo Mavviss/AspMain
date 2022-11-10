7b42eb9a3ace3c0c43f303775625a336b5076dc0
PhoBoKho <0306201431@caothang.edu.vn>
2022-11-10 23:45:34 +0700

    abc

---
+++ Eshop/Eshop/Controllers/AccountsController.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Eshop.Data;
+using Eshop.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Eshop.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private readonly EshopContext _context;
+
+        public AccountsController(EshopContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Accounts
+        public async Task<IActionResult> Index()
+        {
+            var accounts = _context.Accounts.ToList();
+            return View(accounts);
+        }
+
+        // GET: Accounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // GET: Accounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Accounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Username,Password,Email,Phone,Address,FullName,IsAdmin,Avatar,Status")] Account account)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(account);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+            return View(account);
+        }
+
+        // POST: Accounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Username,Password,Email,Phone,Address,FullName,IsAdmin,Avatar,Status")] Account account)
+        {
+            if (id != account.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(account);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccountExists(account.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(account);
+        }
+
+        // GET: Accounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return View(account);
+        }
+
+        // POST: Accounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Accounts == null)
+            {
+                return Problem("Entity set 'EshopContext.Account'  is null.");
+            }
+            var account = await _context.Accounts.FindAsync(id);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccountExists(int id)
+        {
+            return _context.Accounts.Any(e => e.Id == id);
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login([Bind("Username,Password")] Account account)
+        {
+            var accounts = _context.Accounts.FirstOrDefault(a => (account.Username == a.Username && account.Password == a.Password));
+            if (accounts != null)
+            {
+                HttpContext.Session.SetString("User", accounts.FullName);
+                HttpContext.Session.SetInt32("UserId", accounts.Id);
+
+
+                if (accounts.IsAdmin == true)
+                {
+                    HttpContext.Session.SetString("IdUser", "admin");
+                    return RedirectToAction("Index", "Admin", "account");
+                }
+
+                return RedirectToAction("Index", "Products");
+            }
+            else
+            {
+                ViewBag.ErrorMsg = "Login failed!";
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Products");
+        }
+
+
+    }
+}
---
+++ Eshop/Eshop/Models/Invoice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Eshop.Models
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Mã HĐ")]
+        public string Code { get; set; }
+
+        public int AccountId { get; set; }
+
+        // Navigation reference property cho khóa ngoại đến Account 
+        [DisplayName("Khách hàng")]
+        public Account Account { get; set; }
+
+        [DisplayName("Ngày lập")]
+        [DataType(DataType.DateTime)]
+        public DateTime IssuedDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Địa chỉ giao hàng")]
+        [Required(ErrorMessage = "{0} không được bỏ trống")]
+        public string ShippingAddress { get; set; }
+
+        [DisplayName("SĐT giao hàng")]
+        [Required(ErrorMessage = "{0} không được bỏ trống")]
+        [RegularExpression("0\\d{9}", ErrorMessage = "SĐT không hợp lệ")]
+        public string ShippingPhone { get; set; }
+
+        [DisplayName("Tổng tiền")]
+        [DisplayFormat(DataFormatString = "{0:n0}")]
+        [DefaultValue(0)]
+        public int Total { get; set; } = 0;
+
+        [DisplayName("Còn hiệu lực")]
+        [DefaultValue(true)]
+        public bool Status { get; set; } = true;
+
+        // Collection reference property cho khóa ngoại từ InvoiceDetail
+        public List<InvoiceDetail> InvoiceDetails { get; set; }
+    }
+}
